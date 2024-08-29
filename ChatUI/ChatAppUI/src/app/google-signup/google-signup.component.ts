319a8f50f49aa3d98fc770fbe006b41e67c16b8d
Balkrishna Panthi <balkrishnapanthi22@gmail.com>
2024-08-30 02:07:32 +0530

    added google signupcomponent

---
+++ ChatUI/ChatAppUI/src/app/google-signup/google-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-signup',
+  templateUrl: './google-signup.component.html',
+  styleUrls: ['./google-signup.component.css']
+})
+export class GoogleSignupComponent {
+
+}
